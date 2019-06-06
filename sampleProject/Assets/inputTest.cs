e4d573bb68f56be3121801e51333f3da7d3c63ee
memon07 <memonshoyeb26@gmail.com>
2019-06-06 19:17:32 +0530

    input text file added

---
+++ sampleProject/Assets/inputTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int min = 0;
+        int max = 999;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            Debug.Log("Up arrow was pressed");
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            Debug.Log("Up down was pressed");
+
+        if (Input.GetKeyDown(KeyCode.Return))
+            Debug.Log("You hit enter");
+    }
+}
